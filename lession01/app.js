e36b0f5734b72bf34637795f92890512ac54f612
Sandip Pal <sandip.pal18@gmail.com>
2018-01-07 13:51:05 +0530

    Combining filter() and map()

--- lession01/app.js
+++ lession01/app.js
@@ -1,8 +1,25 @@
-const fruits = ['Apple', 'Pear', 'Cherry'];
+const userNames = ['Samir', 'Angela', 'Beatrice', 'Shaniqua', 'Marvin', 'Sean'];
 
-for (let index = 0; index < fruits.length; index++) {
-    const element = fruits[index];
-    console.log(element);
-}
+// Result: [{name: 'Samir'}, {name: 'Shaniqua'}, {name:'Sean'}];
 
-fruits.forEach(fruit => console.log(fruit));
+const userCreate = userNames
+    .filter(name => name.charAt(0) === 'S')
+    .map(name => ({name}));
+
+console.log(userCreate);
+
+const users = [
+    {name: 'Samir', age: 27},
+    {name: 'Angela', age: 33},
+    {name: 'Beatrice', age: 42},
+    {name: 'Shaniqua', age: 30},
+    {name: 'Marvin', age: 23},
+    {name: 'Sean', age: 47}
+  ];
+// Result: ['Angela', 'Beatrice', 'Shaniqua', 'Sean'];
+
+const userUper30 = users
+    .filter(user => user.age >= 30)
+    .map(user => user.name);
+
+console.log(userUper30);
