5558a0156f1ec9dc1c1d00519613869d8b694a67
Sandip Pal <sandip.pal18@gmail.com>
2018-01-07 14:56:24 +0530

    Added Combining filter() and reduce()

--- lession01/app.js
+++ lession01/app.js
@@ -1,8 +1,27 @@
-const fruits = ['Apple', 'Pear', 'Cherry'];
+const products = [
+    { name: 'hard drive', price: 59.99 },
+    { name: 'lighbulbs', price: 2.59 },
+    { name: 'paper towels', price: 6.99 },
+    { name: 'flatscreen monitor', price: 159.99 },
+    { name: 'cable ties', price: 19.99 },
+    { name: 'ballpoint pens', price: 4.49 }
+  ];
+// Result: { name: 'paper towels', price: 6.99 }
 
-for (let index = 0; index < fruits.length; index++) {
-    const element = fruits[index];
-    console.log(element);
-}
+const product = products
+  .filter(product => product.price < 10)
+  .reduce((heighest, product) => {
+      if (heighest.price > product.price) {
+          return heighest;
+      }
+      return product;
+  });
 
-fruits.forEach(fruit => console.log(fruit));
+  console.log(product);
+
+  const total = products
+  .filter(product => product.price > 10)
+  .reduce((sum, product) => sum + product.price, 0)
+  .toFixed(2);
+
+  console.log(total);
