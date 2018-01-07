0146b9d82c41c5795aa8a471de96b4005178a60e
Sandip Pal <sandip.pal18@gmail.com>
2018-01-07 11:00:06 +0530

    Added Reduce Function

--- lession01/app.js
+++ lession01/app.js
@@ -1,8 +1,27 @@
-const fruits = ['Apple', 'Pear', 'Cherry'];
+const total = [ 0, 1, 2, 3 ].reduce(
+    ( acc, cur ) => acc + cur,
+    3
+);
 
-for (let index = 0; index < fruits.length; index++) {
-    const element = fruits[index];
-    console.log(element);
-}
+console.log(total);
 
-fruits.forEach(fruit => console.log(fruit));
+// ==========================================================
+
+const prices = [6.75, 3.10, 4.00, 8.12]; // Total: 21.97
+
+const totalPrice = prices.reduce((sum, price) => sum + price);
+
+console.log(totalPrice);
+
+// ========================================================
+
+const names = ['Gary', 'Pasan', 'Gabe', 'Treasure', 'Gengis', 'Gladys', 'Tony'];
+
+const gNameCount = names.reduce((count, name) => {
+    if (name[0] === 'G') {
+        return count + 1;
+    }
+    return count;
+}, 0);
+
+console.log(gNameCount);
